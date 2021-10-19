6c18276f66050004721b080f40f68b3459b0d35d
Rachana Chavan <racchava@racchava-mn2.linkedin.biz>
2021-10-19 10:17:10 +0530

    Code formatter

--- client/src/main/java/com/example/servingwebcontent/LMSController.java
+++ client/src/main/java/com/example/servingwebcontent/LMSController.java
@@ -15,60 +15,72 @@ import org.springframework.web.client.RestTemplate;
 @Controller
 public class LMSController {
 
-	@Bean
-	public RestTemplate lmsTemplate(RestTemplateBuilder builder) {
-		return builder.build();
-	}
+    @Bean
+    public RestTemplate lmsTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
 
-	@Autowired
-	private RestTemplate lmsTemplate;
-	private String SERVER_URL = "http://127.0.0.1:5000/";
+    @Autowired
+    private RestTemplate lmsTemplate;
+    private String SERVER_URL = "http://127.0.0.1:5000/";
 
 
-	/**
-	 * Serves a html webpage with operations related to OAuth
-	 **/
+    /**
+     * Serves a html webpage with operations related to OAuth
+     **/
 
-	@GetMapping("/lms")
-	public String oauth(@RequestParam(name = "name", required = false, defaultValue = "World") String name, final Model model) {
-		String action = "Please Click login with linkedIn Button to generate access token!";
-		String AUTH_URL = SERVER_URL  + "login";
-		model.addAttribute( "auth_url", AUTH_URL);
-		model.addAttribute( "output", "response" );
-		model.addAttribute( "action", action );
-		return "lms";
-	}
+    @GetMapping("/lms")
+    public String oauth(@RequestParam(name = "name", required = false, defaultValue = "World") String name, final Model model) {
+        String action = "Please Click login with linkedIn Button to generate access token!";
+        String AUTH_URL = SERVER_URL + "login";
+        model.addAttribute("auth_url", AUTH_URL);
+        model.addAttribute("output", "response");
+        model.addAttribute("action", action);
+        return "lms";
+    }
 
-	/**
-	 * Handles the post requests of Html page, calls the API endpoints of server URL.
-	 * To return a response and updates it on UI
-	 **/
+    /**
+     * Handles the post requests of Html page, calls the API endpoints of server URL.
+     * To return a response and updates it on UI
+     **/
 
-	@PostMapping(path = "/lms", produces = {"application/json", "application/xml"}, consumes = {"application/x-www-form-urlencoded"})
-	public String postBody(@RequestBody String POST_ARRAY, final Model model) {
-		String response = "";
+    @PostMapping(path = "/lms", produces = {
+        "application/json",
+        "application/xml"
+    }, consumes = {
+        "application/x-www-form-urlencoded"
+    })
+    public String postBody(@RequestBody String POST_ARRAY, final Model model) {
+        String response = "";
 
-		switch(POST_ARRAY){
-			case "Check_token=Check+Token": response = lmsTemplate.getForObject(SERVER_URL  + "refresh_token", String.class);
-			  break;
-			case "Find_ad_account_by_authenticated_user=Find+Ad+Accounts": response = lmsTemplate.getForObject(SERVER_URL  + "Find_ad_account_by_authenticated_user", String.class);
-				break;
-			case "Get_user_org_access=Find+Ad+Account+roles" : response = lmsTemplate.getForObject(SERVER_URL  + "Get_user_org_access", String.class);
-				break;
-			case "Fetch_ad_account=Fetch+Ad+Account+by+ID": response = lmsTemplate.getForObject(SERVER_URL  + "Fetch_ad_accoun", String.class);
-				break;
-			case "Get_ad_account_user=Get+Ad+Account+User": response = lmsTemplate.getForObject(SERVER_URL  + "Get_ad_account_user", String.class);
-				break;
-			case "Find_ad_account_user_by_account=Find+Ad+Account+Users": response = lmsTemplate.getForObject(SERVER_URL  + "Find_ad_account_user_by_account", String.class);
-				break;
+        switch (POST_ARRAY) {
+            case "Check_token=Check+Token":
+                response = lmsTemplate.getForObject(SERVER_URL + "refresh_token", String.class);
+                break;
+            case "Find_ad_account_by_authenticated_user=Find+Ad+Accounts":
+                response = lmsTemplate.getForObject(SERVER_URL + "Find_ad_account_by_authenticated_user", String.class);
+                break;
+            case "Get_user_org_access=Find+Ad+Account+roles":
+                response = lmsTemplate.getForObject(SERVER_URL + "Get_user_org_access", String.class);
+                break;
+            case "Fetch_ad_account=Fetch+Ad+Account+by+ID":
+                response = lmsTemplate.getForObject(SERVER_URL + "Fetch_ad_accoun", String.class);
+                break;
+            case "Get_ad_account_user=Get+Ad+Account+User":
+                response = lmsTemplate.getForObject(SERVER_URL + "Get_ad_account_user", String.class);
+                break;
+            case "Find_ad_account_user_by_account=Find+Ad+Account+Users":
+                response = lmsTemplate.getForObject(SERVER_URL + "Find_ad_account_user_by_account", String.class);
+                break;
 
-			//Default case statement
-			default: response = "No API calls made!";
-		}
+                //Default case statement
+            default:
+                response = "No API calls made!";
+        }
 
-		model.addAttribute("output", response);
-		model.addAttribute("action", POST_ARRAY);
-		return "lms";
-	}
+        model.addAttribute("output", response);
+        model.addAttribute("action", POST_ARRAY);
+        return "lms";
+    }
 
 }
