fe445727155749c29ffbc666598eb0b1fad5bffb
Rachana Chavan <racchava@racchava-mn2.linkedin.biz>
2021-10-19 17:01:18 +0530

    beautify tool

--- client/src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
+++ client/src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
@@ -1,18 +1,18 @@
 package com.example.servingwebcontent;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 /*
-* Create Spring Boot Application and set a default controller
-*/
+ * Create Spring Boot Application and set a default controller
+ */
 
 @SpringBootApplication
 public final class ServingWebContentApplication {
-    
-    private ServingWebContentApplication(){}
-    public static void main(final String[] args) {
-        SpringApplication.run(ServingWebContentApplication.class, args);
-    }
+
+	private ServingWebContentApplication() {}
+	public static void main(final String[] args) {
+		SpringApplication.run(ServingWebContentApplication.class, args);
+	}
 
 }
