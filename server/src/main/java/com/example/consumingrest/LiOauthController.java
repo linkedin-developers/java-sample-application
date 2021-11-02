a119c75097f5882d4383ce3d77298ecc07312a1b
Rachana Chavan <racchava@racchava-mn2.linkedin.biz>
2021-11-02 09:59:19 +0530

    pom.xml

--- server/src/main/java/com/example/consumingrest/LiOauthController.java
+++ server/src/main/java/com/example/consumingrest/LiOauthController.java
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.linkedin.oauth.builder.ScopeBuilder;
 import com.linkedin.oauth.pojo.AccessToken;
 import com.linkedin.oauth.service.LinkedInOAuthService;
