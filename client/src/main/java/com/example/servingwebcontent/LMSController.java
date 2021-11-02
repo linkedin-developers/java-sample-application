03f5c03631d5f5cea65de964cf89b798cb996004
Rachana Chavan <racchava@racchava-mn2.linkedin.biz>
2021-11-02 16:35:32 +0530

    final

--- client/src/main/java/com/example/servingwebcontent/LMSController.java
+++ client/src/main/java/com/example/servingwebcontent/LMSController.java
@@ -5,7 +5,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
 
