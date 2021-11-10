26efc2162c3573db2e59c3840b4848341739ca2a
Rachana Chavan <racchava@racchava-mn2.linkedin.biz>
2021-11-10 16:28:47 +0530

    parseJSON

--- server/src/main/java/com/example/consumingrest/LiOauthController.java
+++ server/src/main/java/com/example/consumingrest/LiOauthController.java
@@ -1,7 +1,6 @@
 package com.example.consumingrest;
 
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 import java.util.Random;
